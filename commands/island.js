16207940d039387d2a107a61e3ea375439650f47
Marius Begby <mariusbegby@gmail.com>
2018-10-29 10:07:34 +0100

    Added support for multiple Ikariam servers.

--- commands/island.js
+++ commands/island.js
@@ -1,4 +1,4 @@
-exports.run = (bot, msg, args) => {
+exports.run = (server, bot, msg, args) => {
 
   const ika = require('../custom_modules/ika.js');
 
@@ -11,7 +11,7 @@ exports.run = (bot, msg, args) => {
   let x_coord = island_coords[0];
   let y_coord = island_coords[1];
 
-  ika.verifyIslandCoordAndGetId(x_coord, y_coord, (result) =>{
+  ika.verifyIslandCoordAndGetId(msg, x_coord, y_coord, server, (result) =>{
     if(!result) {
       msg.channel.send(`Could not find an island with the coordinations ${x_coord}:${y_coord}. Please try again.`);
     }
@@ -19,7 +19,7 @@ exports.run = (bot, msg, args) => {
       let resource_emotes = ['', '<:wine:421131487992348683>', '<:marble:421131487996542977>', '<:crystal:421131487698747394>', '<:sulfur:471461212568289310>'];
       let wonder_emotes = ['', '<:forge:475413159583416350>', '<:hadesholygrove:475413418740809746>', '<:demetersgarden:475413432619761684>', '<:templeofathene:475413409085784095>', '<:templeofhermes:475413401179521031>', '<:aresstronghold:475413440387874826>', '<:poseidon:475413036253970442>', '<:colossus:475413055371608064>'];
 
-      ika.getIslandInfo(result.id, (islandObject) => {
+      ika.getIslandInfo(msg, result.id, server, (islandObject) => {
 
         message_embed = {
           embed: {
@@ -49,7 +49,16 @@ exports.run = (bot, msg, args) => {
           else {
             message_embed.embed.fields[0].value += `\n**${city.pseudo}** **-** ${city.name} (${city.level}) **-** ${city.army_score_main.format()} MS`;
           }
+
           if(city.state == 2) {inactive_count++;}
+
+          // Discord form body can not be over 1024 chars and adding this will exceed that limit on many islands making the bot not send a message.
+          /*if(city.state == 1){
+            message_embed.embed.fields[0].value += ' <:vacation:421152426427416578>';
+          }
+          if(city.state == 2){
+            message_embed.embed.fields[0].value += ' <:inactive:476253259275960320>';
+          }*/
         });
 
         message_embed.embed.author.name = `[${x_coord}:${y_coord}] ${islandObject.island.name}, ${islandObject.cities.length}/17, ${inactive_count} inactive`;
