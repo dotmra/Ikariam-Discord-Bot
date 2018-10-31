f3e1f24f947cf9505cb67c21078ec698872b03bb
Marius Begby <mariusbegby@gmail.com>
2018-10-31 18:20:18 +0100

    New command !addserver

---
+++ commands/addserver.js
@@ -0,0 +1,43 @@
+exports.run = (guildConf, client, message, args) => {
+  const adminRole = message.guild.roles.find(x => x.name === guildConf.adminRole); // .find("name", guildConf.adminRole);
+  if(!adminRole) return message.reply("Administrator Role Not Found");
+
+  if(!message.member.roles.has(adminRole.id)) {
+    return message.reply("You're not an admin, sorry!");
+  }
+
+  args = args.join(' ');
+
+  servers = {
+    alpha: "Alpha",
+    beta: "Beta",
+    ny: "Ny",
+    omega:"Omega",
+    apollon: "Apollon",
+    asklepios: "Asklepios",
+    boreas: "Boreas",
+    charon: "Charon",
+    demeter: "Demeter",
+    dionysos: "Dionysos",
+    eirene: "Eirene"
+  }
+
+  if (client.settings.get(message.guild.id, "commandMode") === "ALL") {
+    message.channel.send("You have not enabled channel specific servers, to do so, type: \`!globalserver off\`");
+  }
+  else {
+
+    if (!servers.hasOwnProperty(args.toLowerCase())) {
+      message.channel.send(`The Ikariam server \`${args}\` does not exist. Here is a list of valid servers:\nAlpha, Beta, Ny, Omega, Apollon, Asklepios, Boreas, Charon, Demeter, Dionysos, Eirene`);
+    }
+
+    else {
+      guildConf.channelServers[message.channel.id] = servers[args];
+
+      client.settings.set(message.guild.id, guildConf.channelServers, "channelServers");
+      message.channel.send(`The channel \`#${message.channel.name}\` now uses the \`${servers[args]}\` Ikariam server for commands.`);
+
+    }
+  }
+
+}
