b4639b8fb1ff8acb25ec2bb6c39e4465d9d9e64b
Marius Begby <mariusbegby@gmail.com>
2018-10-31 23:52:41 +0100

    Clean up

--- commands/addserver.js
+++ commands/addserver.js
@@ -1,49 +1,41 @@
 exports.run = (guildConf, client, message, args) => {
   const adminRole = message.guild.roles.find(x => x.name === guildConf.adminRole); // .find("name", guildConf.adminRole);
-  if(!adminRole) return message.reply("Administrator Role Not Found");
+  if(!adminRole) return message.channel.send(`A role with the name \`Admin\` not found.`);
 
   if(!message.member.roles.has(adminRole.id)) {
-    return message.reply("You're not an admin, sorry!");
+    return message.channel.send(`You need to have the \`Admin\` role to issue this command!`);
   }
 
   args = args.join(' ');
 
   servers = {
-    alpha: "Alpha",
-    beta: "Beta",
-    ny: "Ny",
-    omega:"Omega",
-    apollon: "Apollon",
-    asklepios: "Asklepios",
-    boreas: "Boreas",
-    charon: "Charon",
-    demeter: "Demeter",
-    dionysos: "Dionysos",
-    eirene: "Eirene"
+    'alpha': 'Alpha',
+    'beta': 'Beta',
+    'ny': 'Ny',
+    'omega': 'Omega',
+    'apollon': 'Apollon',
+    'asklepios': 'Asklepios',
+    'boreas': 'Boreas',
+    'charon': 'Charon',
+    'demeter': 'Demeter',
+    'dionysos': 'Dionysos',
+    'eirene': 'Eirene',
+    'warserver 03': 'Warserver 03'
   }
 
-  if (client.settings.get(message.guild.id, "commandMode") === "ALL") {
+  if (client.settings.get(message.guild.id, 'commandMode') === "ALL") {
     message.channel.send("You have not enabled channel specific servers, to do so, type: \`!globalserver off\`");
   }
   else {
 
-    if (args.toLowerCase() === "warserver 03") {
-      guildConf.channelServers[message.channel.id] = "Warserver 03"
+    if (!servers.hasOwnProperty(args.toLowerCase())) {
+      message.channel.send(`The Ikariam server \`${args}\` does not exist. Here is a list of valid servers:\nAlpha, Beta, Ny, Omega, Apollon, Asklepios, Boreas, Charon, Demeter, Dionysos, Eirene, Warserver 03`);
+    }
+    else {
+      guildConf.channelServers[message.channel.id] = servers[args];
 
       client.settings.set(message.guild.id, guildConf.channelServers, "channelServers");
-      message.channel.send(`The channel \`#${message.channel.name}\` now uses the \`Warserver 03\` Ikariam server for commands.`);
-    }
-
-    else {
-      if (!servers.hasOwnProperty(args.toLowerCase())) {
-        message.channel.send(`The Ikariam server \`${args}\` does not exist. Here is a list of valid servers:\nAlpha, Beta, Ny, Omega, Apollon, Asklepios, Boreas, Charon, Demeter, Dionysos, Eirene`);
-      }
-      else {
-        guildConf.channelServers[message.channel.id] = servers[args];
-
-        client.settings.set(message.guild.id, guildConf.channelServers, "channelServers");
-        message.channel.send(`The channel \`#${message.channel.name}\` now uses the \`${servers[args]}\` Ikariam server for commands.`);
-      }
+      message.channel.send(`The channel \`#${message.channel.name}\` now uses the \`${servers[args]}\` Ikariam server for commands.`);
     }
 
   }
--- commands/globalserver.js
+++ commands/globalserver.js
@@ -1,49 +1,42 @@
 exports.run = (guildConf, client, message, args) => {
   const adminRole = message.guild.roles.find(x => x.name === guildConf.adminRole); // .find("name", guildConf.adminRole);
-  if(!adminRole) return message.reply("Administrator Role Not Found");
+  if(!adminRole) return message.channel.send(`A role with the name \`Admin\` not found.`);
 
   if(!message.member.roles.has(adminRole.id)) {
-    return message.reply("You're not an admin, sorry!");
+    return message.channel.send(`You need to have the \`Admin\` role to issue this command!`);
   }
 
   args = args.join(' ');
 
   servers = {
-    alpha: "Alpha",
-    beta: "Beta",
-    ny: "Ny",
-    omega:"Omega",
-    apollon: "Apollon",
-    asklepios: "Asklepios",
-    boreas: "Boreas",
-    charon: "Charon",
-    demeter: "Demeter",
-    dionysos: "Dionysos",
-    eirene: "Eirene"
+    'alpha': 'Alpha',
+    'beta': 'Beta',
+    'ny': 'Ny',
+    'omega': 'Omega',
+    'apollon': 'Apollon',
+    'asklepios': 'Asklepios',
+    'boreas': 'Boreas',
+    'charon': 'Charon',
+    'demeter': 'Demeter',
+    'dionysos': 'Dionysos',
+    'eirene': 'Eirene',
+    'warserver 03': 'Warserver 03'
   }
 
   if (args.toLowerCase() === "off") {
     client.settings.set(message.guild.id, "CHANNEL", "commandMode");
     client.settings.set(message.guild.id, "", "commandModeAllServer");
-    message.channel.send(`The server is now in \`CHANNEL\` mode. Use \`!addserver (Ikariam Server Name)\` in a channel to enable commands for that server.`);
+    message.channel.send(`The server is now in \`CHANNEL\` mode. Use \`!addserver <Ikariam Server Name>\` in a channel to enable commands for that channel.`);
   }
   else {
-    if (args.toLowerCase() === "warserver 03") {
-      guildConf.channelServers[message.channel.id] = "Warserver 03";
-
-      client.settings.set(message.guild.id, guildConf.channelServers, "channelServers");
-      message.channel.send(`The channel \`#${message.channel.name}\` now uses the \`Warserver 03\` Ikariam server for commands.`);
+    if (!servers.hasOwnProperty(args.toLowerCase())) {
+      message.channel.send(`The Ikariam server \`${args}\` does not exist. Here is a list of valid servers:\nAlpha, Beta, Ny, Omega, Apollon, Asklepios, Boreas, Charon, Demeter, Dionysos, Eirene, Warserver 03`);
     }
     else {
-      if (!servers.hasOwnProperty(args.toLowerCase())) {
-        message.channel.send(`The Ikariam server \`${args}\` does not exist. Here is a list of valid servers:\nAlpha, Beta, Ny, Omega, Apollon, Asklepios, Boreas, Charon, Demeter, Dionysos, Eirene`);
-      }
-      else {
-        client.settings.set(message.guild.id, "ALL", "commandMode");
-        client.settings.set(message.guild.id, servers[args], "commandModeAllServer");
-        client.settings.set(message.guild.id, {}, "channelServers");
-        message.channel.send(`The ikariam server \`${servers[args]}\` will now be used for commands in all channels.`);
-      }
+      client.settings.set(message.guild.id, "ALL", "commandMode");
+      client.settings.set(message.guild.id, servers[args], "commandModeAllServer");
+      client.settings.set(message.guild.id, {}, "channelServers");
+      message.channel.send(`The ikariam server \`${servers[args]}\` will now be used for commands in all channels.`);
     }
   }
 }
