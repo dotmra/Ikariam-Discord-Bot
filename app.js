c870a5ae74d53baa893177212790d2b7d6d74de8
Marius Begby <mariusbegby@gmail.com>
2018-10-31 18:03:06 +0100

    Delete

--- app.js
+++ app.js
@@ -1,18 +1,62 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const RssFeedEmitter = require('rss-feed-emitter');
+const Enmap = require("enmap");
+
+const client = new Discord.Client();
+
 const config = require('./config.json');
-const bot = new Discord.Client();
 const prefix = config.prefix;
+
+client.settings = new Enmap({
+  name: "settings",
+  fetchAll: false,
+  autoFetch: true,
+  cloneLevel: 'deep',
+  dataDir: './data/enmap'
+});
+
+const defaultSettings = {
+  prefix: "!",
+  modLogChannel: "mod-log",
+  modRole: "Mod",
+  adminRole: "Admin",
+  welcomeChannel: "welcome",
+  welcomeMessage: "Say hello to {{user}}, everyone!"
+}
+
+client.on("guildDelete", guild => {
+  // When the bot leaves or is kicked, delete settings to prevent stale entries.
+  client.settings.delete(guild.id);
+});
+
+client.on("guildMemberAdd", member => {
+  // This executes when a member joins, so let's welcome them!
+
+  // First, ensure the settings exist
+  client.settings.ensure(member.guild.id, defaultSettings);
+
+  // Then, get the welcome message using get:
+  let welcomeMessage = client.settings.get(member.guild.id, "welcomeMessage");
+
+  // Our welcome message has a bit of a placeholder, let's fix that:
+  welcomeMessage = welcomeMessage.replace("{{user}}", member.user.tag)
+
+  // we'll send to the welcome channel.
+  member.guild.channels
+    .find("name", client.settings.get(member.guild.id, "welcomeChannel"))
+    .send(welcomeMessage)
+    .catch(console.error);
+});
 
 let feeder = new RssFeedEmitter();
 feeder.add({
   url: 'http://lorem-rss.herokuapp.com/feed', //https://board.us.ikariam.gameforge.com/index.php/BoardFeed/24/
 });
 
-bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.tag}!`);
-  bot.user.setPresence({ status: 'online', game: { name: 'commands', type: 'LISTENING' } })
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({ status: 'online', game: { name: 'commands', type: 'LISTENING' } })
     .catch(console.error);
 
   feeder.on('new-item', function(item) {
@@ -35,101 +79,47 @@ bot.on('ready', () => {
         }
       }
     }
-    //bot.channels.get("491615733785165825").send(message_embed);
+    //client.channels.get("491615733785165825").send(message_embed);
   });
 
 });
 
-bot.on('message', msg => {
+client.on('message', message => {
 
-  if (msg.author.bot) return;
-  if (msg.channel.type === 'dm') return;
+  // This stops if the message is not sent in a guild(Discord Server), and we ignore all bots.
+  if(!message.guild || message.author.bot) return;
 
-  if(msg.content.startsWith(config.prefix)){
-    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+  // We can use ensure() to actually grab the default value for settings,
+  // IF the key doesn't already exist.
+  const guildConf = client.settings.ensure(message.guild.id, defaultSettings);
 
-    try {
-      let commandFile = require(`./commands/${command}.js`);
-      if (msg.channel.parentID == "497449133490307072") {
-        commandFile.run("Eirene", bot, msg, args);
-      }
-      else if (msg.channel.parentID == "490241148875440148") {
-        commandFile.run("Eirene", bot, msg, args);
-      }
-      else if (msg.channel.parentID == "471433407788744714") {
-        commandFile.run("Dionysos", bot, msg, args);
-      }
-      else if (msg.channel.parentID == "501197652013350914") {
-        commandFile.run("Dionysos", bot, msg, args);
-      }
-      else if (msg.channel.parentID == "471433980282011661") {
-        commandFile.run("Dionysos", bot, msg, args);
-      }
-      else if (msg.channel.parentID == "471434430276173835") {
-        commandFile.run("Dionysos", bot, msg, args);
-      }
-      else if (msg.channel.parentID == "471435348719828992") {
-        commandFile.run("Dionysos", bot, msg, args);
-      }
-      else if (msg.channel.parentID == "471435804950921217") {
-        commandFile.run("Dionysos", bot, msg, args);
-      }
-      else if (msg.channel.parentID == "475236737916862465") {
-        commandFile.run("Dionysos", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584026812809216") {
-        commandFile.run("Alpha", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584039227949066") {
-        commandFile.run("Beta", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584055250190356") {
-        commandFile.run("Ny", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584090578812930") {
-        commandFile.run("Omega", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584103765704715") {
-        commandFile.run("Apollon", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584118617735169") {
-        commandFile.run("Asklepios", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584171860099072") {
-        commandFile.run("Boreas", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584195604185119") {
-        commandFile.run("Charon", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584211760644106") {
-        commandFile.run("Demeter", bot, msg, args);
-      }
-      else if (msg.channel.id == "445584303724953610") {
-        commandFile.run("Dionysos", bot, msg, args);
-      }
-      else if (msg.channel.id == "488446025980313611") {
-        commandFile.run("Eirene", bot, msg, args);
-      }
-      else if (msg.channel.id == "449248065887404032") {
-        commandFile.run("Warserver 03", bot, msg, args);
-      }
-      else {
-        return;
-      }
-    } catch (err) {
-      if(err.code == 'MODULE_NOT_FOUND'){
-        return;
-      }
-      console.error(err);
+  // Now we can use the values!
+  // We stop processing if the message does not start with our prefix for this guild.
+  if(message.content.indexOf(guildConf.prefix) !== 0) return;
+
+  const args = message.content.split(/\s+/g);
+  const command = args.shift().slice(guildConf.prefix.length).toLowerCase();
+
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+
+    if (command === "setconf") {
+      commandFile.run(guildConf, client, message, args);
     }
-  }
-  else{
-    return;
+
+    if (command === "showconf") {
+      commandFile.run(guildConf, client, message, args);
+    }
+
+  } catch (err) {
+    if(err.code == 'MODULE_NOT_FOUND') {
+      return;
+    }
+    console.error(err);
   }
 
 });
 
-bot.login(config.token);
+client.login(config.token);
 
-bot.on('error', console.error);
+client.on('error', console.error);
