b6ea41641df769a17c9b7ae1fd51aefc97d9ada8
Marius Begby <mariusbegby@gmail.com>
2018-11-01 16:30:35 +0100

    Better logging to console

--- events/message.js
+++ events/message.js
@@ -21,37 +21,37 @@ module.exports = (client, message, defaultSettings) => {
 
     if (command === "commands") {
       commandFile.run(client, message, args);
-      console.log(`${message.author.tag} issued command !${command} ${args.join(' ')} in server: ${message.guild.name} (${message.guild.id})`);
+      console.log(`${message.author.tag} issued command '${command} ${args.join(' ')}' in server: '${message.guild.name}' (${message.guild.id}) in channel '#${message.channel.name}'`);
     }
 
     if (command === "addserver") {
       commandFile.run(guildConf, client, message, args);
-      console.log(`${message.author.tag} issued command !${command} ${args.join(' ')} in server: ${message.guild.name} (${message.guild.id})`);
+      console.log(`${message.author.tag} issued command '${command} ${args.join(' ')}' in server: '${message.guild.name}' (${message.guild.id}) in channel '#${message.channel.name}'`);
     }
 
     if (command === "globalserver") {
       commandFile.run(guildConf, client, message, args);
-      console.log(`${message.author.tag} issued command !${command} ${args.join(' ')} in server: ${message.guild.name} (${message.guild.id})`);
+      console.log(`${message.author.tag} issued command '${command} ${args.join(' ')}' in server: '${message.guild.name}' (${message.guild.id}) in channel '#${message.channel.name}'`);
     }
 
     if (command === "find") {
       commandFile.run(client, message, args);
-      console.log(`${message.author.tag} issued command !${command} ${args.join(' ')} in server: ${message.guild.name} (${message.guild.id})`);
+      console.log(`${message.author.tag} issued command '${command} ${args.join(' ')}' in server: '${message.guild.name}' (${message.guild.id}) in channel '#${message.channel.name}'`);
     }
 
     if (command === "info") {
       commandFile.run(client, message, args);
-      console.log(`${message.author.tag} issued command !${command} ${args.join(' ')} in server: ${message.guild.name} (${message.guild.id})`);
+      console.log(`${message.author.tag} issued command '${command} ${args.join(' ')}' in server: '${message.guild.name}' (${message.guild.id}) in channel '#${message.channel.name}'`);
     }
 
     if (command === "growth") {
       commandFile.run(client, message, args);
-      console.log(`${message.author.tag} issued command !${command} ${args.join(' ')} in server: ${message.guild.name} (${message.guild.id})`);
+      console.log(`${message.author.tag} issued command '${command} ${args.join(' ')}' in server: '${message.guild.name}' (${message.guild.id}) in channel '#${message.channel.name}'`);
     }
 
     if (command === "island") {
       commandFile.run(client, message, args);
-      console.log(`${message.author.tag} issued command !${command} ${args.join(' ')} in server: ${message.guild.name} (${message.guild.id})`);
+      console.log(`${message.author.tag} issued command '${command} ${args.join(' ')}' in server: '${message.guild.name}' (${message.guild.id}) in channel '#${message.channel.name}'`);
     }
 
   } catch (err) {
@@ -60,13 +60,13 @@ module.exports = (client, message, defaultSettings) => {
         if(err != "DiscordAPIError: Missing Permissions"){
           return console.error(err);
         }
-        return console.log(`Command !${command}: No permission to send message to channel #${message.channel.name} in guild '${message.guild.name}' (DiscordAPIError: Missing Permissions)`);
+        return console.log(`Command '${command} ${args.join(' ')}': No permission to send message to channel '#${message.channel.name}' in guild '${message.guild.name}' (${message.guild.id}) (DiscordAPIError: Missing Permissions)`);
       });
     }
     if(err != "DiscordAPIError: Missing Permissions"){
       return console.error(err);
     }
-    return console.log(`Command !${command}: No permission to send message to channel #${message.channel.name} in guild '${message.guild.name}' (DiscordAPIError: Missing Permissions)`);
+    return console.log(`Command '${command} ${args.join(' ')}': No permission to send message to channel '#${message.channel.name}' in guild '${message.guild.name}' (${message.guild.id}) (DiscordAPIError: Missing Permissions)`);
   }
 
 }
