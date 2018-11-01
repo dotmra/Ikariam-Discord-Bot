8973dfe5877d9fbb672354a8e05e0b4857bf5a99
Marius Begby <mariusbegby@gmail.com>
2018-11-01 14:21:53 +0100

    Better fix for issue #1 DiscordAPIError: Missing Permissions

--- commands/commands.js
+++ commands/commands.js
@@ -17,6 +17,11 @@ exports.run = (message, args) => {
     }
   }
 
-  message.channel.send(message_embed);
+  message.channel.send(message_embed).catch((err) => {
+    if(err != "DiscordAPIError: Missing Permissions"){
+      return console.error(err);
+    }
+    return console.log(`No permission to send message to channel #${message.channel.name} in guild '${message.guild.name}' (DiscordAPIError: Missing Permissions)`);
+  });
 
 }
