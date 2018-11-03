b199e33ec544cf0431bb04b478292a432a682383
Marius Begby <mariusbegby@gmail.com>
2018-11-03 02:20:27 +0100

    No longer needs admin role, it checks for 'ADMINISTRATOR' permission

--- commands/addserver.js
+++ commands/addserver.js
@@ -1,18 +1,11 @@
 exports.run = (guildConf, client, message, args) => {
-  const adminRole = message.guild.roles.find(x => x.name === guildConf.adminRole); // .find("name", guildConf.adminRole);
-  if(!adminRole) return message.channel.send(`A role with the name \`Admin\` not found.`).catch((err) => {
-    if(err != "DiscordAPIError: Missing Permissions"){
-      return console.error(err);
-    }
-    return console.log(`Command !addserver: No permission to send message to channel #${message.channel.name} in guild '${message.guild.name}' (DiscordAPIError: Missing Permissions)`);
-  });
 
-  if(!message.member.roles.has(adminRole.id)) {
-    return message.channel.send(`You need to have the \`Admin\` role to issue this command!`).catch((err) => {
+  if (!message.member.hasPermission('ADMINISTRATOR')) {
+    return message.channel.send(`You need to be an \`Administrator\` in this Discord server to issue this command!`).catch((err) => {
       if(err != "DiscordAPIError: Missing Permissions"){
         return console.error(err);
       }
-      return console.log(`Command !addserver: No permission to send message to channel #${message.channel.name} in guild '${message.guild.name}' (DiscordAPIError: Missing Permissions)`);
+      return console.log(`Command !globalserver: No permission to send message to channel #${message.channel.name} in guild '${message.guild.name}' (DiscordAPIError: Missing Permissions)`);
     });
   }
 
--- events/message.js
+++ events/message.js
@@ -35,7 +35,7 @@ module.exports = (client, message, defaultSettings) => {
     }
 
     if (command === "globalserver") {
-      commandFile.run(guildConf, client, message, args);
+      commandFile.run(client, message, args);
       console.log(`${message.author.tag} issued command '${command} ${args.join(' ')}' in server: '${message.guild.name}' (${message.guild.id}) in channel '#${message.channel.name}'`);
     }
 
@@ -61,12 +61,13 @@ module.exports = (client, message, defaultSettings) => {
 
   } catch (err) {
     if(err.code == 'MODULE_NOT_FOUND') { //if command file is not found, e.g. not a command
-      return message.channel.send(`\`${guildConf.prefix}${command}\` is not a command. To view all commands do \`!commands\``).catch((err) => {
+      return;
+      /*return message.channel.send(`\`${guildConf.prefix}${command}\` is not a command. To view all commands do \`!commands\``).catch((err) => {
         if(err != "DiscordAPIError: Missing Permissions"){
           return console.error(err);
         }
         return console.log(`Command '${command} ${args.join(' ')}': No permission to send message to channel '#${message.channel.name}' in guild '${message.guild.name}' (${message.guild.id}) (DiscordAPIError: Missing Permissions)`);
-      });
+      });*/
     }
     if(err != "DiscordAPIError: Missing Permissions"){
       return console.error(err);
